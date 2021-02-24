5aae86689e749847fb69d09c412d167f1f488c1e
Julien Broyard <broyard.dev@gmail.com>
2021-02-24 21:47:58 +0100

    feat(api): Add Delete endpoint

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -6,6 +6,7 @@ use App\Entity\Pin;
 use App\Form\PinType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,15 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/pin', name: 'pin_', methods: ['GET'])]
 class PinController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
-    public function create(EntityManagerInterface $entityManager, Request $request): Response
+    public function create(Request $request): Response
     {
         $pin = new Pin();
 
@@ -22,8 +30,8 @@ class PinController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($pin);
-            $entityManager->flush();
+            $this->entityManager->persist($pin);
+            $this->entityManager->flush();
 
             return $this->redirectToRoute('app_home');
         }
@@ -33,15 +41,25 @@ class PinController extends AbstractController
         ]);
     }
 
-    #[Route('/edit/{id<[\da-fA-F]{8}\-[\da-fA-F]{4}\-[\da-fA-F]{4}\-[\da-fA-F]{4}\-[\da-fA-F]{12}>}', name: 'edit', methods: ['GET', 'POST'])]
-    public function edit(EntityManagerInterface $entityManager, Pin $pin, Request $request): Response
+    #[Route('/delete/{id<[\da-fA-F]{8}\-[\da-fA-F]{4}\-[\da-fA-F]{4}\-[\da-fA-F]{4}\-[\da-fA-F]{12}>}', name: 'delete', methods: ['DELETE'])]
+    public function delete(Pin $pin): RedirectResponse
     {
-        $form = $this->createForm(PinType::class, $pin);
+        $this->entityManager->remove($pin);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
+
+    }
+
+    #[Route('/edit/{id<[\da-fA-F]{8}\-[\da-fA-F]{4}\-[\da-fA-F]{4}\-[\da-fA-F]{4}\-[\da-fA-F]{12}>}', name: 'edit', methods: ['GET', 'PUT'])]
+    public function edit(Pin $pin, Request $request): Response
+    {
+        $form = $this->createForm(PinType::class, $pin, ['method' => 'PUT']);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($pin);
-            $entityManager->flush();
+            $this->entityManager->persist($pin);
+            $this->entityManager->flush();
 
             return $this->redirectToRoute('pin_show', ['id' => $pin->getId()]);
         }
